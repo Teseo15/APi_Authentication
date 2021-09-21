0b7de5f52de0e89f867705fb9b952b2876b2a96a
Teseo15 <brayan.blas.egaovil30@gmail.com>
2021-09-21 16:51:03 -0500

    1:30

--- src/app.js
+++ src/app.js
@@ -2,14 +2,15 @@ import express from 'express';
 import morgan from 'morgan';
 import { createRoles } from './libs/initialSetup';
 import pkg from '../package.json'
+
 import productsRoute from './routes/products.routes';
 import authRoutes from './routes/auth.routes';
 
 const app = express();
-createRoles();
+
 
 app.set('pkg',pkg);
-
+createRoles();
 //Registra los detalles de la solicitud
 app.use(morgan('dev')); 
 //Para que pueda leer datos json
